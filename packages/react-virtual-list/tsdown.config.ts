63d59cf5212a7b964cda65b0ece5b8b95b78d442
Andrii Tereshchuk <andriy9335@gmail.com>
2026-08-04 19:09:19 +0200

    update build config

--- packages/react-virtual-list/tsdown.config.ts
+++ packages/react-virtual-list/tsdown.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-
-  format: ["esm", "cjs"],
-
+  format: ["esm"],
   dts: true,
-
   clean: true,
-
   sourcemap: true,
-
   external: ["react", "react-dom", "react/jsx-runtime"],
 });
