9b1d7212450e858f71f6cd672d9ac58123e1cea9
Andrii Tereshchuk <andriy9335@gmail.com>
2026-08-04 22:24:14 +0200

    chore: setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,34 @@
+// eslint.config.mjs
+
 import js from "@eslint/js";
 import globals from "globals";
-
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-
+import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-
 import prettier from "eslint-config-prettier";
 
-export default [
+export default tseslint.config(
   {
-    ignores: ["**/dist/**", "**/node_modules/**", "**/.changeset/**"],
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/*.tgz",
+      "**/.changeset/**",
+    ],
   },
 
   js.configs.recommended,
 
+  ...tseslint.configs.recommended,
+
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{ts,tsx,js,jsx}"],
+
+    ...react.configs.flat.recommended,
+    ...react.configs.flat["jsx-runtime"],
 
     languageOptions: {
-      parser: tsParser,
-
+      ...react.configs.flat.recommended.languageOptions,
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -29,21 +36,8 @@ export default [
     },
 
     plugins: {
-      "@typescript-eslint": tsPlugin,
-
-      react,
-
+      ...react.configs.flat.recommended.plugins,
       "react-hooks": reactHooks,
-    },
-
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-
-      ...react.configs.recommended.rules,
-
-      ...reactHooks.configs.recommended.rules,
-
-      "react/react-in-jsx-scope": "off",
     },
 
     settings: {
@@ -51,7 +45,14 @@ export default [
         version: "detect",
       },
     },
+
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
   },
 
   prettier,
-];
+);
