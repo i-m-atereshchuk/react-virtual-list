a25bf05d1d3809c0fb5184692abf04d683ef367f
Andrii Tereshchuk <andriy9335@gmail.com>
2026-08-04 20:21:18 +0200

    chore: configure TypeScript workspace resolution

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "react-virtual-list": fileURLToPath(
+        new URL("../packages/react-virtual-list/src/index.ts", import.meta.url),
+      ),
+    },
+  },
 });
